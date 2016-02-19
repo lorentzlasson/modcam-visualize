b1fd6b136dba9b141a82919d60ac47fc639dc521
Robert Barlin <robert.barlin@gmail.com>
2016-02-19 11:13:12 +0100

    stuff

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,8 @@
+Date.prototype.getWeek = function() {
+	var onejan = new Date(this.getFullYear(), 0, 1);
+  return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7) - 1;
+}
+
 function set(direction, value){
 	countMap[direction] = value
 	render()
