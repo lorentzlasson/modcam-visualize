b2f5a54f8f0c39a8ae265dcff648b3b2205cf16e
Robert Barlin <robert.barlin@gmail.com>
2016-02-25 15:00:04 +0100

    stuff

--- public/js/demographics.js
+++ public/js/demographics.js
@@ -1,12 +1,18 @@
 var allChart = undefined
+var pieChart = undefined
+var resCount = 0
 var allMen = []
 var allWomen = []
 var db = 'https://2ed64fa0-abba-4d60-8437-62d7bcad30af-bluemix.cloudant.com/demographicdb/_design/data/_view/'
+var categories = ['0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']
 var today = new Date()
 today.setHours(0)
 today.setMinutes(0)
 today.setSeconds(0)
 today.setMilliseconds(0)
+
+var selectedGender = 'all'
+var selectedHour = '12'
 
 var selectedDate = new Date(today)
 selectedDate.setDate(23)
@@ -14,24 +20,36 @@ selectedDate.setDate(23)
 $(document).ready(function(){
 	$('#men').on('click', function(){
 		showMen()
+		selectedGender = 'men'
 		setSelected('men')
-		
+		updatePieChart()
 	})
 
 	$('#women').on('click', function(){
 		showWomen()
+		selectedGender = 'women'
 		setSelected('women')
+		updatePieChart()
 	})
 
 	$('#all').on('click', function(){
 		showAll()
+		selectedGender = 'all'
 		setSelected('all')
+		updatePieChart()
+	})
+
+	$('#hour-picker').change(function(){
+		console.log('change')
+		var v = $('#hour-picker option:selected').val()
+		console.log(v)
+		selectedHour = v
+		updatePieChart()
 	})
 
 	createCharts()
 	getAllMen()
 	getAllWomen()
-
 })
 
 function setSelected(sel){
@@ -41,6 +59,7 @@ function setSelected(sel){
 
 function createCharts(){
 	createAllChart()
+	createPieChart()
 }
 
 function mapData(arr, data){
@@ -52,8 +71,55 @@ function mapData(arr, data){
 			z: m.value
 		}
 		arr.push(d)
-		console.log(Date.UTC(m.key[0], (parseInt(m.key[1])-1), m.key[2], m.key[3]))
 	}
+}
+
+function createPieChart(){
+	pieChart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'container-two',
+			type: 'pie',
+			plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+		},
+		title: {
+			text: ''
+		},
+		plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                showInLegend: true
+            }
+        },
+		series: [
+			{
+				name: 'Ages', 
+				colorByPoint: true, 
+				dataLabels: {
+				  formatter:function() {
+				    if(this.y != 0) {
+				      return this.y;
+				    }
+				  }
+				},
+				data: [
+					{name: '0', y: 0},
+					{name: '10', y: 0},
+					{name: '20', y: 0},
+					{name: '30', y: 0},
+					{name: '40', y: 0},
+					{name: '50', y: 0},
+					{name: '60', y: 0},
+					{name: '70', y: 0},
+					{name: '80', y: 0},
+					{name: '90', y: 0},
+					{name: '100', y: 0}
+				]
+			}
+		]
+	})
 }
 
 function createAllChart(){
@@ -63,7 +129,7 @@ function createAllChart(){
 			type: 'bubble'
 		},
 		title: {
-			text: 'Demo-graphics'
+			text: ''
 		},
 		xAxis: {
 			text: 'Hour',
@@ -96,7 +162,123 @@ function showMen(){
 	allChart.series[0].show()
 }
 
+function updateMenWigets(){
+	var most = findMost(allMen)
+	if (most){
+		$('#most-men').html(most.key[3] + ':00 | ' + most.value + ' ' + (most.value < 2 ? 'man' : 'men'))
+	} else {
+		$('#most-men').html(' - | - ')
+	}
+	
 
+}
+
+function updateWomenWidgets(){
+	var most = findMost(allWomen)
+	if (most){
+		$('#most-women').html(most.key[3] + ':00 | ' + most.value + ' ' + (most.value < 2 ? 'woman' : 'women'))
+	} else {
+		$('#most-women').html(' - | - ')
+	}
+}
+
+
+function findMost(data){
+	var most = undefined
+	for (var i in data){
+		if (!most || most.value < data[i].value){
+			most = data[i]
+		}
+	}
+
+	return most
+}
+
+function totalVisit(){
+	var total = 0
+	for (var i in allMen){
+		total += allMen[i].value
+	}
+
+	for (var i in allWomen){
+		total += allWomen[i].value
+	}
+
+	return total
+}
+
+function findMostOfAges(){
+	var ageMap = {}
+	var most = undefined
+
+	for (var i in allMen){
+		var d = allMen[i]
+		var k = d.key[4]
+		ageMap[k] = ageMap[k] + d.value || d.value
+	}
+
+	for (var i in allWomen){
+		var d = allWomen[i]
+		var k = d.key[4]
+		ageMap[k] = ageMap[k] + d.value || d.value
+	}
+
+	for (var i in ageMap){
+		if (!most || most.value < ageMap[i]){
+			most = {value: ageMap[i], age: i }
+		}
+	}
+
+	return most
+}
+
+function groupAges(time){
+	ages = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+	ageGroup = []
+	for (var i in ages){
+		ageGroup.push(0)
+	}
+
+	if (selectedGender != 'women'){
+		console.log('men')
+		for (var i in allMen){
+			var m = allMen[i]
+			var k = m.key
+
+			if (k[3] == time){
+				var group = Math.floor((k[4] / 10))
+				ageGroup[group] += m.value
+			}
+		
+		}
+	}
+	
+	if (selectedGender != 'men'){
+		console.log('women')
+		for (var i in allWomen){
+			var m = allWomen[i]
+			var k = m.key
+
+			if (k[3] == time){
+				var group = (k[4] / 10).toFixed()
+				ageGroup[group] += m.value
+			}
+		}
+	}
+
+	return ageGroup
+}
+
+function updateMostOfAge(){
+	var most = findMostOfAges()
+	var total = totalVisit()
+	if (most){
+		var percent = most.value / total
+		$('#most-age').html('Age: ' + most.age + ' | ' + (percent*100) + ' % of total visitors')
+	} else {
+		$('#most-age').html(' - | - ')
+	}
+}	
 
 function updateMenCharts(){
 	var d = []
@@ -113,10 +295,13 @@ function updateWomenCharts(){
 function getAllWomen(){
 	var doc = 'all_women?reduce=true&group_level=5'
 	$.getJSON(db+doc, function(data){
-		console.log(data)
 		if (data.rows){
+			resCount++
 			allWomen = data.rows
-			updateWomenCharts()
+			if (resCount == 2){
+				resCount = 0
+				update()
+			}	
 		}
 	})
 }
@@ -124,10 +309,34 @@ function getAllWomen(){
 function getAllMen(){
 	var doc = 'all_men?reduce=true&group_level=5'
 	$.getJSON(db+doc, function(data){
-		console.log(data)
 		if (data.rows){
+			resCount++
 			allMen = data.rows
-			updateMenCharts()
+
+			if (resCount == 2){
+				resCount = 0
+				update()
+			}
 		}
 	})
+}
+
+function update(){
+	updateWomenCharts()
+	updateWomenWidgets()
+	updateMostOfAge()
+	updateMenCharts()
+	updateMenWigets()
+	updatePieChart()
+}
+
+function updatePieChart(){
+	var ages = groupAges(selectedHour)
+
+	var setData = []
+	for (var i in categories){
+		setData.push([categories[i], ages[i]])
+	}
+	console.log(setData)
+	pieChart.series[0].setData(setData)
 }
