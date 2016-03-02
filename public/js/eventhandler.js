6e43512aa10514b41b0de8b6d1d7014f5124e27f
Simon Lagos <simonlagos@simons-mbp-4.mma.se.ibm.com>
2016-03-02 16:19:05 +0100

    Display events

--- public/js/eventhandler.js
+++ public/js/eventhandler.js
@@ -1,5 +1,11 @@
+var socketAdr = 'ws://dwell-node-red.mybluemix.net/ws/events'
+var socket = undefined;
+var connectFails = 0;
+var hadConnection = false;
+
 $(document).ready(function(){
 	eventDropdown();
+	connectSocket();
 });
 function showAndHide(array, value){
 	for(var i = 0; i < array.length; i++){
@@ -12,4 +18,61 @@ function eventDropdown(){
 		var options = ['dwell', 'limit', 'in', 'out'];
 		showAndHide(options, this.value);
 	});
+}
+
+function socketOnOpen(){
+	console.log('Websocket open')
+	if (hadConnection) return;
+	hadConnection = true;
+	connectFails = 0;
+	var eventRequest = JSON.stringify({req: "get"})
+	socket.send(eventRequest)
+}
+
+function socketOnMessage(evt){
+	var data = JSON.parse(evt.data);
+	if (data.res == "get"){
+		console.log("get");
+		$.each(data.events.dwell, function(key, val){
+				$('#table-in tr:last').after('<tr><td>'+ val.area +'</td><td>'+ val.day +'</td><td>'+ val.time +'</td><td>'+ val.action +'</td><td><span>X</span></td></tr>');
+		});
+		$.each(data.events.limit, function(key, val){
+				$('#table-limit tr:last').after('<tr><td>'+ val.area +'</td><td>'+ val.day +'</td><td>'+ val.limit +'</td><td>'+ val.action +'</td><td><span>X</span></td></tr>');
+		});
+		$.each(data.events.in, function(key, val){
+				$('#table-in tr:last').after('<tr><td>'+ val.area +'</td><td>'+ val.day +'</td><td>'+ val.action +'</td><td><span>X</span></td></tr>');
+		});
+		$.each(data.events.out, function(key, val){
+				$('#table-in tr:last').after('<tr><td>'+ val.area +'</td><td>'+ val.day +'</td><td>'+ val.action +'</td><td><span>X</span></td></tr>');
+		});
+
+	}
+	else if (data.res == "live"){
+		console.log(data);
+		$.each(data.result, function(key, val){
+			$('#table-log tr:last').after('<tr><td>'+ val.area +'</td><td>'+ val.day +'</td><td>'+ val.action +'</td><td><span>X</span></td></tr>');
+		});
+	}
+}
+
+function socketOnClose(){
+	console.log('Websocket close')
+	if (3 > connectFails++){
+		connectSocket()
+	} else {
+		console.log("Tried connecting to socket to many times")
+	}
+}
+
+function connectSocket(){
+	try {
+		socket = new WebSocket(socketAdr)
+
+		socket.onopen = socketOnOpen
+		socket.onmessage = socketOnMessage
+		socket.onclose = socketOnClose
+
+	} catch(exception){
+		console.log('Failed to connect websocket')
+	}
 }
