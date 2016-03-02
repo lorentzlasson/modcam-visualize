a10cc120a5cbf31759d77fe7dd1a9f377477259f
erdalbej <erdbej@gmail.com>
2016-03-02 13:21:11 +0100

    stuff

--- public/js/eventhandler.js
+++ public/js/eventhandler.js
@@ -1,23 +1,15 @@
 $(document).ready(function(){
-	$('#event').on('change', function() {
-		var options = ['dwell', 'limit', 'in', 'out'];
-		if(this.value == "dwell"){
-			showAndHide(options, this.value);
-		}
-		else if(this.value == "limit"){
-			showAndHide(options, this.value);
-		}
-		else if(this.value == "in"){
-			showAndHide(options, this.value);
-		}
-		else if(this.value == "out"){
-			showAndHide(options, this.value);
-		}
-	});
+	eventDropdown();
 });
 function showAndHide(array, value){
 	for(var i = 0; i < array.length; i++){
 		$('#' + array[i]).hide();
 	}
 	$('#' + value).show();
+}
+function eventDropdown(){
+	$('#event').on('change', function() {
+		var options = ['dwell', 'limit', 'in', 'out'];
+		showAndHide(options, this.value);
+	});
 }
