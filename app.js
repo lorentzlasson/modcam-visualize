ae7789da71f89adfbe68ef68ce9baee5a133bd90
Lorentz Lasson <lasson.lorentz@gmail.com>
2016-03-10 10:29:10 +0100

    simplify server startup code

--- app.js
+++ app.js
@@ -5,7 +5,5 @@ var appEnv = cfenv.getAppEnv();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(appEnv.port, '0.0.0.0', function() {
-
-  console.log("server starting on " + appEnv.url);
-});
+app.listen(appEnv.port)
+console.log("server starting on " + appEnv.url);
