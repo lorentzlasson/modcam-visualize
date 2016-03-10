3b1ba892fec870294ea8d502f81a986641fe4fdc
Lorentz Lasson <lasson.lorentz@gmail.com>
2016-03-10 13:18:33 +0100

    export http server

--- app/server/index.js
+++ app/server/index.js
@@ -1,6 +1,8 @@
 var express = require('express')
+var http = require('http')
 var app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-module.exports = app
+var server = http.createServer(app)
+module.exports = server
