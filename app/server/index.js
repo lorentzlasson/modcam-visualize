d17b16b11402d2401de243c589dacce007c4e663
Lorentz Lasson <lasson.lorentz@gmail.com>
2016-03-10 15:46:38 +0100

    add api to broadcast message via ws

--- app/server/index.js
+++ app/server/index.js
@@ -1,6 +1,13 @@
 var express = require('express')
 var http = require('http')
 var app = express()
+var websocket = require('../websocket')
+
+app.post('/broadcast', (req, res) => {
+	var message = req.query.message
+	websocket.broadcast(message)
+	res.end()
+})
 
 app.use(express.static(__dirname + '/public'))
 
