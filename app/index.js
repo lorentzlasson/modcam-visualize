c1169f6ff59df30609c9565a36be4b68848c512c
Lorentz Lasson <lasson.lorentz@gmail.com>
2016-03-10 13:25:59 +0100

    add dummy ws server

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 var cfenv = require('cfenv')
 var appEnv = cfenv.getAppEnv()
 var server = require('./server')
+var websocket = require('./websocket')
+
 server.listen(appEnv.port)
+websocket.run()
+
 console.log('server starting on ' + appEnv.url)
---
+++ app/websocket/index.js
@@ -0,0 +1,23 @@
+var WebSocketServer = require('ws').Server
+var server = require('../server')
+var wss = new WebSocketServer({server, path: '/ws/counter'})
+
+var run = () => {
+	wss.on('connection', function(ws) {
+		var id = setInterval(function() {
+			ws.send(JSON.stringify(process.memoryUsage()), function() { /* ignore errors */ })
+		}, 1000)
+		console.log('started client interval')
+		ws.on('message', function(data) {
+			console.log('message received: %s', data)
+		})
+		ws.on('close', function() {
+			console.log('stopping client interval')
+			clearInterval(id)
+		})
+	})
+}
+
+module.exports = {
+	run
+}
