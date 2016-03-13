2fee9676ae40e16fbe9dd3f99e34624598b8db96
Lorentz Lasson <lasson.lorentz@gmail.com>
2016-03-13 18:33:14 +0100

    split up functions in db module

--- app/database/peoplecount.js
+++ app/database/peoplecount.js
@@ -4,24 +4,23 @@ var db = cloudant.use('counterdb')
 var util = require('../util')
 
 var getByDay = (date) => {
+	var dateSplit = util.splitDate(date)
+	var startkey = dateSplit.slice(0)
+	startkey.push(0)
+	var endkey = dateSplit.slice(0)
+	endkey.push(1)
+
+	return getByDate(startkey, endkey)
+}
+
+var getByDate = (startkey, endkey) => {
 	return new Promise((resolve, reject) => {
-		date = new Date(date)
-		if(!util.isValidDate(date))
-			return reject('invalid date')
-
-		var dateSplit = util.splitDate(date)
-		var startkey = dateSplit.slice(0)
-		startkey.push(0)
-		var endkey = dateSplit.slice(0)
-		endkey.push(1)
-
 		var params = {
 			reduce: true,
 			group_level: 4,
 			startkey,
 			endkey
 		}
-
 		debug('params: %j', params)
 
 		db.view('data', 'total_by_date', params, (err, body) => {
