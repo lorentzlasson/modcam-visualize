3919459c6052d8bc42dd285f2bcfcddfb582cd66
Lorentz Lasson <lasson.lorentz@gmail.com>
2016-03-13 18:36:35 +0100

    validate date in router

--- app/server/routes/peoplecount.js
+++ app/server/routes/peoplecount.js
@@ -1,9 +1,17 @@
 var express = require('express')
 var router = express.Router()
+var util = require('../../util')
 var db = require('../../database/peoplecount')
 
 router.get('/day/:date', (req, res) => {
-	db.getByDay(req.params.date)
+	var date = new Date(req.params.date)
+	if(!util.isValidDate(date)){
+		return res.status(400).json({
+			error: 'invalid date'
+		})
+	}
+
+	db.getByDay(date)
 	.then((counts) => {
 		res.json({
 			counts
